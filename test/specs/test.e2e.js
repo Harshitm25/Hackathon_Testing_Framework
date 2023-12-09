e31abfceb8b241f08cdfbfc67187288ed1e0624e
User <user@192.168.1.3>
2023-12-09 15:53:04 +0530

    signup page updated

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,11 +1,22 @@
-const signup=require('../pageobjects/signup.page')
+const signup = require('../pageobjects/signup.page')
 
 describe('Webpage Test Suite', () => {
-    it('should create an account successfully', async () => {
-        await browser.url('https://65742d2560517d40bec1210e--steady-boba-f77e2d.netlify.app/');
+    before(() => {
+        browser.url('https://65742d2560517d40bec1210e--steady-boba-f77e2d.netlify.app/');
+    });
 
+    it('should show error message for empty fields', async () => {
+        const createAccountButton = await signup.btnSubmit;
+        await createAccountButton.click();
+        await browser.pause(3000)
+        const errorMessage = await signup.errorMessage;
+        errorMessage.waitForExist();
+        await expect(errorMessage).toHaveText('Please fill in all the fields.');
+    });
+
+    it('should create an account successfully', async () => {
         const emailInput = await signup.emailInput;
-        const passwordInput = await signup.passwordInput ;
+        const passwordInput = await signup.passwordInput;
         const usernameInput = await signup.usernameInput;
         const createAccountButton = await signup.btnSubmit;
 
@@ -13,22 +24,11 @@ describe('Webpage Test Suite', () => {
         await passwordInput.setValue('password123');
         await usernameInput.setValue('testuser');
         await createAccountButton.click();
-        await browser.pause(5000)
+        await browser.pause(2000)
         const successMessage = await signup.successMessage;
         await successMessage.waitForExist();
         await expect(successMessage).toHaveText('Account created for testuser with email: test@example.com');
     });
 
-    it('should show error message for empty fields', async () => {
-        await browser.url('https://65742d2560517d40bec1210e--steady-boba-f77e2d.netlify.app/');
 
-        const createAccountButton = await $('button');
-        await createAccountButton.click();
-
-        const errorMessage = await signup.errorMessage;
-        errorMessage.waitForExist();
-
-        const errorText = await errorMessage.getText();
-        await expect(errorMessage).toHaveText('Please fill in all the fields.');
-    });
 });
